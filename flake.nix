826dffad72bfc854e5078db82ccd86203952be44
System administrator <omkar-mohanty>
2025-05-18 13:51:15 +0530

    should work

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
-  description = "A simple NixOS flake";
+  description = "NixOS configuration";
 
-   inputs = {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # home-manager, used for managing user configuration
     home-manager = {
@@ -14,24 +14,28 @@
     };
   };
 
-  outputs = { self, home-manager, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      # TODO please change the hostname to your own
+      my-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
 
-	home-manager.nixosModules.home-manager {
-		 home-manager.useGlobalPkgs = true;
-            	 home-manager.useUserPackages = true;
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-            	# TODO replace ryan with your own username
-            	home-manager.users.omkar = import ./home.nix;
+            # TODO replace ryan with your own username
+            home-manager.users.omkar = import ./home.nix;
 
-	}
-      ];
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
     };
   };
 }
