9bbc1dbb0ae401658c310375e67bc557b84d3b9c
System administrator <omkar-mohanty>
2025-05-19 11:48:57 +0530

    formatted home.nix

--- home.nix
+++ home.nix
@@ -8,19 +8,22 @@
 {
 
   # TODO please change the username & home directory to your own
-  home.username = "omkar";
-  home.homeDirectory = "/home/omkar";
+  home = {
+    username = "omkar";
+    homeDirectory = "/home/omkar";
+    packages = with pkgs; [
+      inputs.nixvim.packages.${pkgs.system}.default
+      git
+      gcc
+      ripgrep
+      wget
+      google-chrome
+    ];
+    stateVersion = "24.11";
+  };
 
   # Packages that should be installed to the user profile.
 
-  home.packages = with pkgs; [
-    inputs.nixvim.packages.${pkgs.system}.default
-    git
-    gcc
-    ripgrep
-    wget
-    google-chrome
-  ];
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
@@ -29,14 +32,15 @@
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.11";
 
   # Let home Manager install and manage itself.
-  programs.home-manager.enable = true;
 
-  programs.git = {
-    enable = true;
-    userName = "omkar-mohanty";
-    userEmail = "franzohouser@gmail.com";
+  programs = {
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      userName = "omkar-mohanty";
+      userEmail = "franzohouser@gmail.com";
+    };
   };
 }
